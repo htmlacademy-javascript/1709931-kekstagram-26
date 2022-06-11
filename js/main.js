9968bcb7e9981beb70891c31e01b59b96e5da4ba
Anastasia Kiseleva <aakkiseleva@gmail.com>
2022-06-11 20:28:12 +0300

    Исправлены ошибки в функциях

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 // Генерация случайного целого положительного числа из переданного диапазона включительно
 // https://learn.javascript.ru/number
 
-const randomInteger = (min, max) => {
+const getRandomInteger = (min, max) => {
   if (min >= 0 && max > min) {
     return Math.floor(min + Math.random() * (max + 1 - min));
   }
-  throw new Error ('Аргументы функции должны быть положительными и/или аргумент max должен быть больше min');
+  return 0;
 };
 
-randomInteger(0, 10);
+getRandomInteger(0, 5);
 
 // Проверка максимальной длины строки
-const stringLength = (string, maxLength) => string.length <= maxLength;
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
-stringLength('aaaa', 1);
+checkStringLength('aaaa', 1);
