a63ae752723d9b527c81f8c7dd6eda9b9bb0eac2
Keks <keks@htmlacademy.ru>
2022-06-14 22:51:27 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -13,20 +13,19 @@ const checkStringLength = (string, maxLength) => string.length <= maxLength;
 checkStringLength('aaaa', 1);
 
 
-const TOTAL_OBJECTS = [];
+const photoIds = [];
 for (let i = 1; i <= 25; i++) {
-  TOTAL_OBJECTS.push(i);
+  photoIds.push(i);
 }
 
 // Функция, которая генерирует массив с неповторяющимися числами
-// https://ru.stackoverflow.com/questions/624726/%D0%9F%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C-20-%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D1%8B%D1%85-%D1%87%D0%B8%D1%81%D0%B5%D0%BB-%D0%BE%D1%82-1-%D0%B4%D0%BE-1000-%D0%BD%D0%BE-%D1%87%D1%82%D0%BE%D0%B1%D1%8B-%D1%8D%D1%82%D0%B8-%D1%87%D0%B8%D1%81%D0%BB%D0%B0-%D0%BD%D0%B5-%D0%BF%D0%BE%D0%B2%D1%82%D0%BE%D1%80%D1%8F%D0%BB%D0%B8%D1%81%D1%8C
-const createArray = (array) => array.splice(Math.random()*array.length, 1)[0];
+const getId = (items) => items.splice(getRandomInteger(0, items.length - 1), 1)[0];
 
 // Функция генерации массива объектов с комментарием к фотографии
 
-const TOTAL_COMMENTS = [];
+const commentIds = [];
 for (let i = 1; i <= 1000; i++) {
-  TOTAL_COMMENTS.push(i);
+  commentIds.push(i);
 }
 
 const COMMENTS_MESSAGES = [
@@ -48,21 +47,29 @@ const COMMENTS_NAMES = [
   'Вашингтон',
 ];
 
+const PHOTOS_COUNT = 25;
+
+const Avatar = {
+  MIN: 1,
+  MAX: 6
+};
+
+const Like ={
+  MIN: 15,
+  MAX: 200
+};
+
 // Функция, которая создает объект - комментарий
 const createComment = () => ({
-  id: createArray(TOTAL_COMMENTS),
-  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-  message: COMMENTS_MESSAGES[getRandomInteger(0, 5)],
-  name: COMMENTS_NAMES[getRandomInteger(0, 5)]
+  id: getId(commentIds),
+  avatar: `img/avatar-${getRandomInteger(Avatar.MIN, Avatar.MAX)}.svg`,
+  message: COMMENTS_MESSAGES[getRandomInteger(0, COMMENTS_MESSAGES.length - 1)],
+  name: COMMENTS_NAMES[getRandomInteger(0, COMMENTS_NAMES.length - 1)]
 });
-
-// Создаем массив из нескольких объектов - комментариев
-const commentsPublished = Array.from({length: 3}, createComment);
-
 
 // Функция генерации объекта - описания фотографии, опубликованной пользователем.
 
-const PHOTO_URL = TOTAL_OBJECTS.map((elements) => `photos/${elements}.jpg`);
+const photoUrl = photoIds.map((elements) => `photos/${elements}.jpg`);
 
 const PHOTO_DESCRIPTIONS = [
   'Это котик',
@@ -72,14 +79,14 @@ const PHOTO_DESCRIPTIONS = [
 ];
 
 const createObject = () => ({
-  id: createArray(TOTAL_OBJECTS),
-  url: createArray(PHOTO_URL),
-  description: PHOTO_DESCRIPTIONS[getRandomInteger(0, 3)],
-  likes: getRandomInteger(15, 200),
-  comments: commentsPublished
+  id: getId(photoIds),
+  url: getId(photoUrl),
+  description: PHOTO_DESCRIPTIONS[getRandomInteger(0, PHOTO_DESCRIPTIONS.length - 1)],
+  likes: getRandomInteger(Like.MIN, Like.MAX),
+  comments: Array.from({length: getRandomInteger(1, 10)}, createComment)
 });
 
-const photoPublished = Array.from({length: 25}, createObject);
+const photoPublished = Array.from({length: PHOTOS_COUNT}, createObject);
 console.log(photoPublished);
 
 // photoPublished.forEach((photo) => {
